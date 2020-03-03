cb92b804c0e28431408d88dd9626668c882d1026
ambiyo-marion <ambiyomarion@gmail.com>
2020-03-03 15:04:32 +0300

    updated the body

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// This is for the slideshow
 var slideIndex = 2;
 showSlides(slideIndex);
 
@@ -24,4 +23,43 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function myFunction() {
+    if(weWork.style.display === "none") {
+        weWork.style.display = "block";
+    } else {
+        weWork.style.display = "none";
+  
+}
+}
+function myFunction1() {
+    var rooms = document.getElementById("card-rooms");
+    if(rooms.style.display === "none") {
+        rooms.style.display = "block";
+    } else {
+        rooms.style.display = "none";
+
+    }
+    
+}
+function myFunction2() {
+    var kiongozi = document.getElementById("card-kiongozi");
+    if(kiongozi.style.display === "none") {
+        kiongozi.style.display = "block";
+    } else {
+        kiongozi.style.display = "none";
+
+    }
+    
+}
+function myFunction3() {
+    var workaholic = document.getElementById("card-workaholic");
+    if(workaholic.style.display === "none") {
+        workaholic.style.display = "block";
+    } else {
+        workaholic.style.display = "none";
+
+    }
+
 }
